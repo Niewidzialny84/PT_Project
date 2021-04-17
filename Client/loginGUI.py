eb307d49a2a150b5aed039ab8a1d74dca34e1739
CatOnXTC <karolsienkiewicz03@gmail.com>
2021-04-17 22:14:28 +0200

    cosmetic changes + hardcode language

--- Client/loginGUI.py
+++ Client/loginGUI.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 
+#Ui of the Main Window
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         #Main Settings
@@ -15,27 +16,33 @@ class Ui_MainWindow(object):
         self.centralwidget.setFont(font)
         self.centralwidget.setObjectName("centralwidget")
 
+        #Quit Button
         self.quit_Button = QtWidgets.QPushButton(self.centralwidget)
         self.quit_Button.setGeometry(QtCore.QRect(410, 0, 41, 31))
         self.quit_Button.setObjectName("quit_Button")
+        self.quit_Button.setText("X")
         self.quit_Button.clicked.connect(lambda:MainWindow.close())
 
+        #Minimize Window Button
         self.minimize_Button = QtWidgets.QPushButton(self.centralwidget)
         self.minimize_Button.setGeometry(QtCore.QRect(370, 0, 41, 31))
         self.minimize_Button.setObjectName("minimize_Button")
+        self.minimize_Button.setText("_")
         self.minimize_Button.clicked.connect(lambda:MainWindow.showMinimized())
 
+        #Top Window Bar
         self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.horizontalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 371, 31))
         self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
         self.horizontalLayout_11 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout_11.setContentsMargins(0, 0, 0, 0)
         self.horizontalLayout_11.setObjectName("horizontalLayout_11")
         self.app_Name = QtWidgets.QLabel(self.horizontalLayoutWidget)
-        self.app_Name.setCursor(QtGui.QCursor(QtCore.Qt.OpenHandCursor))
         self.app_Name.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
         self.app_Name.setObjectName("app_Name")
+        self.app_Name.setText(" Najfajniejszy Komunikator")
         self.horizontalLayout_11.addWidget(self.app_Name)
+
+        #Left login side
         self.verticalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 40, 201, 79))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
@@ -50,7 +57,10 @@ class Ui_MainWindow(object):
         self.verticalLayout_3.addWidget(self.password_Text)
         self.login_Button = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.login_Button.setObjectName("login_Button")
+        self.login_Button.setText("Login")
         self.verticalLayout_3.addWidget(self.login_Button)
+
+        #Right register side
         self.verticalLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
         self.verticalLayoutWidget_2.setGeometry(QtCore.QRect(240, 40, 201, 133))
         self.verticalLayoutWidget_2.setObjectName("verticalLayoutWidget_2")
@@ -71,10 +81,15 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.addWidget(self.mail_Text)
         self.register_Button = QtWidgets.QPushButton(self.verticalLayoutWidget_2)
         self.register_Button.setObjectName("register_Button")
+        self.register_Button.setText("Register")
         self.verticalLayout_4.addWidget(self.register_Button)
+
+        #Language Button
         self.language_Button = QtWidgets.QPushButton(self.centralwidget)
         self.language_Button.setGeometry(QtCore.QRect(10, 150, 201, 23))
         self.language_Button.setObjectName("language_Button")
+        self.language_Button.setText("Polski")
+        self.language_Button.clicked.connect(lambda:self.change_Language())
 
         #Lines
         #Middle Line
@@ -92,32 +107,37 @@ class Ui_MainWindow(object):
 
         MainWindow.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.quit_Button.setText(_translate("MainWindow", "X"))
-        self.minimize_Button.setText(_translate("MainWindow", "_"))
-        self.app_Name.setText(_translate("MainWindow", "Najfajniejszy Komunikator"))
-        self.login_Button.setText(_translate("MainWindow", "Login"))
-        self.register_Button.setText(_translate("MainWindow", "Register"))
-        self.language_Button.setText(_translate("MainWindow", "Polish Language"))
+    def change_Language(self):
+        if(self.language_Button.text() == "Polski"):
+            self.language_Button.setText("English")
+            self.login_Button.setText("Zaloguj się")
+            self.register_Button.setText("Zarejestruj się")
+        else:
+            self.language_Button.setText("Polski")
+            self.login_Button.setText("Login")
+            self.register_Button.setText("Register")
 
+#MainWindow overrides
 class Window(QtWidgets.QMainWindow):
     def mousePressEvent(self, QMouseEvent):
         if QMouseEvent.button()==QtCore.Qt.LeftButton:
             self.m_flag=True
             self.m_Position=QMouseEvent.globalPos()-self.pos()
+            self.setCursor(QtGui.QCursor(QtCore.Qt.ClosedHandCursor))
             QMouseEvent.accept()
             
     def mouseMoveEvent(self, QMouseEvent):
         if QtCore.Qt.LeftButton and self.m_flag:  
                 self.move(QMouseEvent.globalPos()-self.m_Position)
         QMouseEvent.accept()
-    
 
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.m_flag=False
+        self.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+    
+#Core
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = Window()
