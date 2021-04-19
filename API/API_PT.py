1c0b6ab9dfb524b174dbd69967122143db9c1f4d
96peterharris <96peterharris@gmail.com>
2021-04-19 21:09:38 +0200

    Added date field in history_content table and update schema

--- API/API_PT.py
+++ API/API_PT.py
@@ -4,6 +4,7 @@ from flask_marshmallow import Marshmallow
 from flask_restful import Resource, Api
 from sqlalchemy.orm import validates, Session
 from marshmallow import fields, validate
+from datetime import datetime
 
 
 app = Flask(__name__) 
@@ -178,7 +179,8 @@ class HistoryContent(db.Model):
     db.Column('id', db.Integer, primary_key=True), 
     db.Column('history_id', db.Integer, db.ForeignKey('history.id')),
     db.Column('username', db.String(64), db.ForeignKey('user.username')),
-    db.Column('content', db.Text))
+    db.Column('content', db.Text),
+    db.Column('date', db.DateTime, nullable=False, default=datetime.utcnow))
 
     def __init__(self, history_id, username, content):
         self.history_id = history_id
@@ -187,7 +189,7 @@ class HistoryContent(db.Model):
 
 class HistoryContentSchema(ma.Schema):
     class Meta:
-        fields = ('history_id', 'username', 'content')
+        fields = ('history_id', 'username', 'content', 'date')
 
 historyContent_schema = HistoryContentSchema() 
 historiesContent_schema = HistoryContentSchema(many=True)
