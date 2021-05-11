15bd2d6e8e16ea3563ce0d9a1c6636a8b32176e6
CatOnXTC <karolsienkiewicz03@gmail.com>
2021-05-11 18:46:31 +0200

    small login check change

--- Client/login_Check.py
+++ Client/login_Check.py
@@ -4,19 +4,34 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 def login_Check(nick, password,language):
-    pattern = re.compile("^(?=.*[A-Z])(?=.*\d)(?=.*[a-z])[A-Za-z\d]{8,}$")
-    if(pattern.match(password)):
-        return True
+    nick_pattern = re.compile("^.+$")
+    password_pattern = re.compile("^(?=.*[A-Z])(?=.*\d)(?=.*[a-z])[A-Za-z\d]{8,}$")
+    if(nick_pattern.match(nick)):
+        if(password_pattern.match(password)):
+            return True
+        else:
+            if(language=="Polski"):
+                message = QMessageBox()
+                message.setWindowTitle("Error")
+                message.setIcon(QMessageBox.Critical)
+                message.setText("The password has to contain at least 8 characters, a upper case letter, a lower case letter and a digit!")
+                message.exec_()
+            else:
+                message = QMessageBox()
+                message.setWindowTitle("Błąd")
+                message.setIcon(QMessageBox.Critical)
+                message.setText("Hasło musi zawierać przynajmniej 8 znaków, jedną wielką literę, jedną małą literę i cyfrę!")
+                message.exec_()
     else:
         if(language=="Polski"):
             message = QMessageBox()
             message.setWindowTitle("Error")
             message.setIcon(QMessageBox.Critical)
-            message.setText("The password has to contain at least 8 characters, a upper case letter, a lower case letter and a digit!")
+            message.setText("No username given!")
             message.exec_()
         else:
             message = QMessageBox()
             message.setWindowTitle("Błąd")
             message.setIcon(QMessageBox.Critical)
-            message.setText("Hasło musi zawierać przynajmniej 8 znaków, jedną wielką literę, jedną małą literę i cyfrę!")
+            message.setText("Nie podano nazwy użytkownika!")
             message.exec_()
